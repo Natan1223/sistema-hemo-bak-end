eadc9ea41b0711663b50ec769524e559a7749c84
Nata dos Santos Bandeira <nata.bandeira@mns.hemoam>
2020-09-03 14:48:29 -0400

    Atualização de informações README

--- env.php
+++ env.php
@@ -2,10 +2,10 @@
 
 putenv('DISPLAY_ERROS_DETAIL='.true);
 
-putenv('BD_POSTGRESQL_HOST=localhost'); 
-putenv('BD_POSTGRESQL_DBNAME=');
-putenv('BD_POSTGRESQL_USER=');
-putenv('BD_POSTGRESQL_PASSWORD=');
-putenv('BD_POSTGRESQL_PORT=');
+putenv('BD_POSTGRESQL_HOST=90.0.3.231'); 
+putenv('BD_POSTGRESQL_DBNAME=bd-hemo-uea');
+putenv('BD_POSTGRESQL_USER=postgres');
+putenv('BD_POSTGRESQL_PASSWORD=Hemoam2020');
+putenv('BD_POSTGRESQL_PORT=3434');
 
-putenv('JWT_SECRET_KEY=');
+putenv('JWT_SECRET_KEY= Projeto HEMO UEA/HEMOAM');
