281664587c6b6b7c6728311136a49a60f7887da9
Nata dos Santos Bandeira <nata.bandeira@mns.hemoam>
2020-09-03 15:11:46 -0400

    Arquivos de criação de banco de dados, schemas e tabelas

---
+++ Script-BancoDeDados/CriarBanco.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE "bd-hemo-uea"
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'C.UTF-8'
+       LC_CTYPE = 'C.UTF-8'
+       CONNECTION LIMIT = -1;
