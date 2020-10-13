34ff850bf951e2358e49db53b1f88c8b09e69c20
Nata dos Santos Bandeira <nata.bandeira@mns.hemoam>
2020-10-13 16:54:21 -0400

    .

--- App/Controllers/PostgreSQL/AttendanceController.php
+++ App/Controllers/PostgreSQL/AttendanceController.php
@@ -29,67 +29,6 @@ class AttendanceController
         return $response;
     }
 
-    public function registerCity(Request $request, Response $response, array $args): Response
-    {
-        $data = $request->getParsedBody();
-
-        //reject empty strings
-        if ($data['description'] == ""){
-            $response = $response
-                ->withStatus(406)
-                ->withjson([
-                    "message" => [
-                        "pt" => "Erro ao cadastrar cidade...",
-                        "en" => "Error registering city."
-                    ],
-                    'result' => null
-                ]);
-
-            return $response;
-        }
-
-        $cityDAO = new CityDAO();
-        $city = new CityModel();
-
-        if($data){
-            $city
-            ->setDescription ($data['description']);
-
-            $idCity = $cityDAO->registerCity($city); 
-            
-            if($idCity){
-                $response = $response->withjson([
-                    "message" => [
-                        "pt" => "Cidade cadastrada com sucesso...",
-                        "en" => "City register was successful."
-                    ],
-                    'result' => null
-                ]);
-            }else {
-                $response = $response
-                ->withStatus(406)
-                ->withjson([
-                    "message" => [
-                        "pt" => "Erro ao cadastrar cidade...",
-                        "en" => "Error registering city."
-                    ],
-                    'result' => null
-                ]);
-            }
-        }else{
-            $response = $response
-            ->withStatus(406)
-            ->withjson([
-                "message" => [
-                    "pt" => "Parametros não aceitaveis...",
-                    "en" => "Unacceptable parameters."
-                ],
-                'result' => null
-            ]);
-        }
-    
-        return $response;
-    }
 
     
 }
